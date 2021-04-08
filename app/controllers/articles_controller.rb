1102ccb00a95505a2284caa849da2612ea85ed1b
Ross <ross.lillis@gmail.com>
2021-04-09 09:12:18 +1000

    commit local project

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,66 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+
+  def show
+  end
+
+  def index
+    @articles = Article.paginate(page: params[:page], per_page: 5 )
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+  end
+
+  def create
+    byebug
+    # render plain: params[:article]  # To render object saved by form to screen
+    @article = Article.new(article_params)
+    @article.user = current_user 
+    # render plain: @article.inspect
+    if @article.save
+      flash[:notice] = 'Article was created successfully'
+      redirect_to article_path(@article) # or just @article (show action instance variable)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @article.update(article_params)
+      flash[:notice] = 'Article was updated successfully'
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description, category_ids: [])
+  end
+
+  def require_same_user
+    if current_user != @article.user && !current_user.admin?
+      flash[:alert] = "You can only edit and delete your own article"
+      redirect_to @article
+    end
+  end
+  
+end
